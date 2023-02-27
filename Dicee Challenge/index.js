69ae179f465bfba176ee065b28a37c08d0f82dc3
No1KnowMe <99428156+No1KnowMe@users.noreply.github.com>
2023-02-27 20:08:13 +0500

    Update index.js

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,5 +1,5 @@
-var player1 = Math.round(Math.random()*6) + 1;
-var player2 = Math.round(Math.random()*6) + 1;
+var player1 = Math.round(Math.random()*5) + 1;
+var player2 = Math.round(Math.random()*5) + 1;
 
 var img1 = document.querySelector("img.img1").setAttribute("src", "images/dice"+player1+".png");
 var img2 = document.querySelector("img.img2").setAttribute("src", "images/dice"+player2+".png");
